a2bb4dae02a7cba0ddc8377d823440aa83093b2b
Bruno Santos <brunosps00@gmail.com>
2021-06-15 00:55:22 -0300

    Todo App Initial Commit

---
+++ lib/todo_app.rb
@@ -0,0 +1,6 @@
+require "todo_app/version"
+
+module TodoApp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/todo_app_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe TodoApp do
+  it "has a version number" do
+    expect(TodoApp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
